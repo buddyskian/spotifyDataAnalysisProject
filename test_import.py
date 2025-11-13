f0e52e990b5db745641628b50185d75207f8b5fc
buddyskian <ianianian@fastmail.com>
2025-11-13 14:42:08 -0500

    Comments and code cleanup.

--- test_import.py
+++ test_import.py
@@ -1,9 +1,13 @@
 import json
 import createOutput as co
+
+#RATE LIMIT IS 50 SONGS PER BATCH CALL
+#MAX CALLS IS 30 PER SECOND, THROTTLE FOR LARGE SCALE
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-# Replace with your actual Client ID and Client Secret
+# API Keys are pulled from json file to avoid leaking to github
+# IMPORTANT: make sure to switch between keys from the file to avoid hitting rate limits
 with open('APICodes.json') as codeFile:
     codes = json.load(codeFile)
     CLIENT_ID = codes[2]['clientid']
@@ -15,28 +19,55 @@ auth_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIEN
 sp = spotipy.Spotify(auth_manager=auth_manager)
 
 
-#RATE LIMIT IS 50 SONGS PER BATCH CALL
 
+#read spotify streaming history file and return
+# dict() holding all the streams and their relevant information
+# dict() holding all the songs streamed and the information aquried from the api call
+#TODO: fix the docstrings for this funciton, possibly split into two functions for readability
 def dataFileRead(inFileName):
+    """
+    This function takes an input json file and returns the data on every indvidual steam and the
+    information on each song streamed from the spotify API.
+    Args:
+        inFileName (str): the name of a json file formatted as detailed in $makeAFileFormatFile$, without the .json at the end of the string.
+    Returns:
+        tuple (dict(), dict()): A tuple containing:
+            -streams : the dict holding information on all streams, keyed by stream date.
+            -songDatabase : the dict holding information on all streams, keyed by stream date.
+    
+    """
     inFile = 'Data/'+inFileName+'.json'
     with open(inFile) as f:
         dataRead = json.load(f)
-    parsedData = dict()
-    #set that will be used for the api batch call
+    
+    #dict will hold information on every stream in the input file, indexed by streamdate
+    streams = dict()
+    
+    #set that will be used for the api batch call, holds songURIs
     allSongURI = set()
+
+    #will hold the data aquired from spotipy API calls, indexed by songURI
     songDatabase = dict() #songURI:(name,albumName,artists,albumCoverURL,duration)
     for value in dataRead:
-        curTrack = value['master_metadata_track_name']
+        #if stream is not a song, next stream
         if (value['spotify_track_uri'] is None or value['spotify_track_uri'] == None) : continue
-        parsedData.update({curTrack: parsedData.get(curTrack, 0)+1})
-        print(curTrack, ":", value['spotify_track_uri'])
-        #Optimize this to not require calling the API every time
-        #This will avoid hitting the rate limit and also hopfully prevent a need to multi thread.
-        allSongURI.add(value['spotify_track_uri'])
-        #trackTime = sp.track(value['spotify_track_uri'])['duration_ms']
 
-    print(len(allSongURI), "\n\n")
+        #add relevant stream information to streams dict
+        #TODO: make this into a function
+
+
+
+        #add song to the cache for the batch call to the spotipy API 
+        #this avoids redundant calls
+        allSongURI.add(value['spotify_track_uri'])
+        
+
+    #While this batch call avoids redundant calls, it doesn't limit itself to 50 calls at a time
+    #TODO: Limit number of calls per batch to 50 and limit number of calls/second to 50
     songImports = sp.tracks(allSongURI)['tracks']
+    
+    #fill songDatabase, could be made into a separate function later
+    #TODO: Make this into a callable function most likely
     for song in songImports:
         uri = song['uri']
         name = song['name']
@@ -48,6 +79,7 @@ def dataFileRead(inFileName):
         artistsString = artistsString[:-2]
         albumCoverURL = song['album']['images'][0]['url']
         songDatabase.update({uri:(name, albumName, artistsString, albumCoverURL, duration)})
+    
     return True
 
 #main
