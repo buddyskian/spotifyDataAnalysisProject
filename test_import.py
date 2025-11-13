e5579d21ad624d0a47ae78fd980b7c2e462d99c3
buddyskian <ianianian@fastmail.com>
2025-11-13 11:10:51 -0500

    Optimized the API calls using a set as a cache to hopefully avoid rate limiting myself during testing.

--- test_import.py
+++ test_import.py
@@ -14,20 +14,28 @@ auth_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIEN
 sp = spotipy.Spotify(auth_manager=auth_manager)
 
 
+#RATE LIMIT IS 50 SONGS PER BATCH CALL
+
 def dataFileRead(inFileName):
     inFile = 'Data/'+inFileName+'.json'
     with open(inFile) as f:
         dataRead = json.load(f)
     parsedData = dict()
+    #set that will be used for the api batch call
+    allSongURI = set()
     for value in dataRead:
         curTrack = value['master_metadata_track_name']
         if (value['spotify_track_uri'] is None or value['spotify_track_uri'] == None) : continue
         parsedData.update({curTrack: parsedData.get(curTrack, 0)+1})
         print(curTrack, ":", value['spotify_track_uri'])
-        trackTime = sp.track(value['spotify_track_uri'])['duration_ms']
-        print(curTrack, ":", trackTime)
+        #Optimize this to not require calling the API every time
+        #This will avoid hitting the rate limit and also hopfully prevent a need to multi thread.
+        allSongURI.add(value['spotify_track_uri'])
+        #trackTime = sp.track(value['spotify_track_uri'])['duration_ms']
 
-    print(parsedData)
+    print(len(allSongURI))
+    allSongData = sp.tracks(allSongURI)
+    print(allSongData)
     return True
 
 #main
